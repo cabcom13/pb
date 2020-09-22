548dc50ea5d479c1d54d5872296ba9e5ed16e231
cabcom13 <cabcom13@gmail.com>
2020-09-22 23:43:55 +0200

    add icons

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// vue.config.js
+module.exports = {
+    pwa: {
+        name: 'PrayBook',
+        themeColor: '#ab47bc',
+        msTileColor: '#ab47bc',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: '#ab47bc',
+      
+    }
+  }
